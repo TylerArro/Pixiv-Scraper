3959a762649712ddad95ff894b538d640d98462b
Arro <arrot23@gmail.com>
2022-06-13 02:55:05 +0200

    Got all image URLS - Selenium to proceed

--- Scarper/pixivScraper.py
+++ Scarper/pixivScraper.py
@@ -3,9 +3,28 @@ from bs4 import BeautifulSoup
 import pixCookies
 
 cookies = pixCookies.COOKIES
-
 headers = pixCookies.HEADERS
-
+imgHeaders = pixCookies.IMG_HEADERS
+imageLimit = 1
+urlBase = "https://www.pixiv.net"
 response = requests.get('https://www.pixiv.net/ranking.php', cookies=cookies, headers=headers)
 soup = BeautifulSoup(response.content, 'html.parser')
-print(soup)
+#print(soup.prettify())
+
+imageHTML = soup.find_all("a",class_="work _work",limit=imageLimit)
+
+for x in imageHTML:
+    #looks like im going to have to use selenium here to get the image url
+
+    #print(x.attrs['href'])
+
+    urlSuffix = x.attrs['href']
+    print(urlBase + urlSuffix)
+
+    responseImg = requests.get(urlBase + urlSuffix, cookies=cookies, headers=imgHeaders)
+    soupImg = BeautifulSoup(responseImg.content, 'html.parser')
+    print(soupImg.prettify())
+
+    #imageURL = soupImg.find_all("div" , id="root")
+    #print(imageURL)
+
