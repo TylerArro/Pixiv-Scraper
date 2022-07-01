ca15ddc1d61290b9c5cd3fd3883c35a4fc7356ae
Arro <arrot23@gmail.com>
2022-07-01 14:11:59 +0200

    Created Zip file with all images - rankings

--- Webapp/functions.py
+++ Webapp/functions.py
@@ -1,13 +1,33 @@
+from calendar import firstweekday
+from email.mime import base
 import glob
+from zipfile import ZipFile
+import os
+from os.path import basename
 
-# figure out how to use glob.glob instead of i glob sorted(glob.glob("Webapp/static/28793893/*.jpg"), key=os.path.getmtime)
+
+# figure out how to use glob.glob instead of iglob sorted(glob.glob("Webapp/static/28793893/*.jpg"), key=os.path.getmtime)
 def getImglist(pixivID):
     print(pixivID)
     img_list = []
+    noImgs = 0
     for filename in glob.iglob("Webapp/static/"+ pixivID +"/*.jpg"):
         print(filename)
         img_list.append(filename.split("\\")[-1].split(".")[0])
-        
-    return  img_list
+        noImgs += 1
+    return  img_list,noImgs
 
+def createZip(pixivID,noImgs):
+
+    if os.path.exists("Webapp/zips/"+ str(pixivID) + '_' + str(noImgs) + '.zip'):
+        print("Updates zip file already exists")
+        return
+    with ZipFile( "Webapp/zips/"+ str(pixivID) + '_' + str(noImgs) + '.zip','w') as zipObj:
+        for folderName,subfolders,filenames in os.walk("Webapp/static/"+ pixivID):
+            for filename in filenames:
+                filePath = os.path.join(folderName,filename)
+                zipObj.write(filePath,basename(filePath))
+    
+    print("ZIP created with " + str(noImgs) + " images")
+    return
 #print(getImglist(28793893))
--- Webapp/webapp.py
+++ Webapp/webapp.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import requests
 import os
-from functions import getImglist
+from functions import getImglist, createZip
 from classes import pixImg
 
 #on click start scraper then display the scraped images in the returned HTML
@@ -14,13 +14,12 @@ def home_page():
 
 @app.route('/rankings')
 def rankings():
-    imgIDs = '98962415.png'
-    imgURL = 'static/rankings/' + imgIDs
-    artworkID = '98962415'
     img_list = []
-    imgIDs = getImglist('rankings')
+    imgIDs,noImgs = getImglist('rankings')
     for img in imgIDs:
         img_list.append(pixImg('static/rankings/' + img + ".jpg", img))
+    createZip('rankings',noImgs)
+    
     return render_template('rankings.html',img_list = img_list)
 
 @app.route('/users', methods = ['POST'])
@@ -28,10 +27,12 @@ def users():
     pixivID = request.form['pixiv-id']
     img_list = []
     #loop through images and get URL add them to img class list
-    imgIDs = getImglist(pixivID)
+    imgIDs,noImgs = getImglist(pixivID)
     for img in imgIDs:
         img_list.append(pixImg('static/' + pixivID + '/' + img + ".jpg", img))
     return render_template('users.html', pixivID = pixivID,img_list = img_list)
+
+
 # 0.0.0.0 to make website avalible on public ip
 #default port 5000
 if __name__ == '__main__':
